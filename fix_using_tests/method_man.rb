0529ba68b5b4bb08b8bfc74ecfe5112f601db44e
Kevin Garcia <kg.gar91@gmail.com>
2020-06-08 21:35:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,9 @@
 require 'pry'
 
 def start_game(player1, player2)
-  print "Hello #{player1} & #{player2}"
+  p "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
-  print "#{player1} is better than #{player2}"
+  p "#{player1} is better than #{player2}"
 end
