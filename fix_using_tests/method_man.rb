112347b79824ca65b9aa6585ef7a60deb1d6ce68
Kevin Garcia <kg.gar91@gmail.com>
2020-06-08 21:22:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,10 +2,12 @@
 require 'pry'
 
 def start_game(player1, player2)
+  binding.pry
   puts "Hello #{"player1"} & #{"playar2"}"
 end
 
 def play_game(player 1, player2)
+  binding.pry
   puts "#{player1} is better than #{player2}"
 end
 end
