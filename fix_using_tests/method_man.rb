119d34213e093a136d30aaea0fb1c8ab5b01dbb5
Kevin Garcia <kg.gar91@gmail.com>
2020-06-08 21:18:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def start_game('player1', 'player2')
-  puts "Hello #{player1} & #{playar2}"
+def start_game("player1", "player2")
+  puts "Hello #{"player1"} & #{"playar2"}"
 end
 
 def play_game('player 1', 'player2')
