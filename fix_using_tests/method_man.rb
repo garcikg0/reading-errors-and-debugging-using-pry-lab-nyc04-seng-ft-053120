0911863be0fa9e7c4fc9c1044cea15a985f843bd
Kevin Garcia <kg.gar91@gmail.com>
2020-06-08 21:14:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,11 @@
 # don't forget to add: require 'pry'
 require 'pry'
+
 def start_game("player1", "player2")
   puts "Hello #{player1} & #{playar2}"
 end
 
 def play_game("player 1" , "player2")
   puts "#{player1} is better than #{player2}"
+end
 end
